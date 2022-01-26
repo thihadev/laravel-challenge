3a7428757889faf1341f5ad2e4f631ee8de0cca5
naingminkhant <naingminkhant.cm@gmail.com>
2022-01-26 19:02:55 +0630

    post reaction toggle api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -24,6 +25,54 @@ class PostController extends Controller
         }
         return response()->json([
             'data' => $data,
+        ]);
+    }
+    
+    public function toggleReaction(Request $request)
+    {
+        $request->validate([
+            'post_id' => 'required|int|exists:posts,id',
+            'like'   => 'required|boolean'
+        ]);
+        
+        $post = Post::find($request->post_id);
+        if(!$post) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'model not found'
+            ]);
+        }
+        
+        if($post->user_id == auth()->id()) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'You cannot like your post'
+            ]);
+        }
+        
+        $like = Like::where('post_id', $request->post_id)->where('user_id', auth()->id())->first();
+        if($like && $like->post_id == $request->post_id && $request->like) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'You already liked this post'
+            ]);
+        }elseif($like && $like->post_id == $request->post_id && !$request->like) {
+            $like->delete();
+            
+            return response()->json([
+                'status' => 200,
+                'message' => 'You unlike this post successfully'
+            ]);
+        }
+        
+        Like::create([
+            'post_id' => $request->post_id,
+            'user_id' => auth()->id()
+        ]);
+        
+        return response()->json([
+            'status' => 200,
+            'message' => 'You like this post successfully'
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,6 @@ Route::post('login', [LoginController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     
     Route::get('posts', [PostController::class, 'list']);
+    Route::post('posts/reaction', [PostController::class, 'toggleReaction']);
     
 });
