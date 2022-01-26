45fab6781c107375da9dd4c6402e676adff62459
naingminkhant <naingminkhant.cm@gmail.com>
2022-01-26 20:29:54 +0630

    add payroll function in staff controller

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\EmployeeManagement\Staff;
 use Illuminate\Http\Request;
 
 class StaffController extends Controller
 {
-    //
+    protected $staff;
+    
+    public function __construct(Staff $staff)
+    {
+        $this->staff = $staff;
+    }
+    
+    public function payroll()
+    {
+        $data = $this->staff->salary();
+    
+        return response()->json([
+            'data' => $data
+        ]);
+    }
 }
