00b23d66fd97e8a42cbcd2d403a2968ad3fcc305
Thihasoe <thihasoe@comquas.com>
2022-02-26 23:51:52 +0630

    login API refactor

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,51 +5,26 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Validator;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        if (!$request->email) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'email is required'
-            ]);
-        }
-        
-        if(strlen($request->email) < 6) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'email is invalid'
-            ]);
-        }
-    
-        if (!$request->password) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'password is required'
-            ]);
-        }
-        if(strlen($request->password) < 8) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'password is invalid'
-            ]);
-        }
-    
-        $user = User::where('email', $request->email)->first();
-        if (!$user) {
-            return response()->json([
-                'status'  => 404,
-                'message' => 'Model not found.'
-            ]);
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|string|min:6',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->first()], 422);
+        } 
+        if (!$user = User::where('email', $request->email)->first()) {
+            return response()->json(['message' => 'User Not Found'], 404);
         }
     
         if (!Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'status'  => 404,
-                'message' => 'Invalid credentials'
-            ]);
+            return response()->json(['message' => 'Invalid credentials'], 404);
         }
         
         return response()->json([
